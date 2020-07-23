8874c6db67106293383158d364c567a672b8b246
nishuChandravanshi <nishu181099@gmail.comgit config --global user.email nishu181099@gmail.comgit config --global user.email nishu181099@gmail.com>
2020-07-23 17:23:57 +0530

    99:Recover BST modified sol with single pass and O(1) aux space added

--- TREE/LC:99 - Recover BST (from initial bst with two nodes swapped).cpp
+++ TREE/LC:99 - Recover BST (from initial bst with two nodes swapped).cpp
@@ -9,6 +9,62 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+			//T:O(n) (Single Pass), S:O(1)
+class Solution {
+public:
+    void inorderTraversal(TreeNode* root, TreeNode **first, TreeNode **mid, TreeNode **last, TreeNode  **prev) //**-> for keeping address of the pointer type var (as these are already pointers)
+    {
+        if(root == NULL) return;
+        
+        inorderTraversal(root->left, first, mid, last, prev);
+       
+        if( *prev and root->val < (*prev)->val)
+        {
+           if(! *first)
+           {
+               *first = *prev;
+               *mid = root;
+           }
+            else
+                *last = root;
+            
+        }
+        *prev = root;
+        
+        if(*first and *last) return;
+        
+        inorderTraversal(root->right, first, mid, last, prev);
+        
+    }
+    
+    void recoverTree(TreeNode* root) {
+
+        if(root == NULL or (root->left == NULL and root->right == NULL)) return;
+        
+        TreeNode *prev, *first, *mid, *last;
+        prev = first = mid = last = NULL;
+        
+        inorderTraversal(root, &first, &mid, &last, &prev); //passing addr of pointer so as the changes made by the func must be reflected in the original pointers as well
+        
+        if(first and last)
+            swap(first->val, last->val);
+        else if(first and mid)
+            swap(first->val, mid->val);
+ 
+    }
+};
+
+/*  T:O(n) (Single Pass)
+    idea- inorder of bst => sorted
+    The inorder of given bst there can be either two or one point where sort will be violated
+    -two points where curr is less than prev => two non adjacent nodes swapped --> making first point to the prev node and last will then point to next node which for which we'll encounter curr < prev
+    -only one such point => two adjacent nodes are swapped-->first will point to prev and middle will point to curr and these two will be the required nodes (in this case last will remain NULLs)
+
+*/
+
+
+
+/*		//T:O(n), S:O(n)
 class Solution {
 public:
     void inorderTraversal(TreeNode* root, vector<int>& inorder, bool& toModify, int &index)
@@ -65,7 +121,7 @@ public:
         
     }
 };
-
+*/
 /*  T:O(n), S:O(n)
     idea- inorder of bst => sorted
     The inorder of given bst there can be either two or one point where sort will be violated
