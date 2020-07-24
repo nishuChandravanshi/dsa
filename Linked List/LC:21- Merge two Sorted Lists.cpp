32fb37eea163c7c266453051c39899e73faca9df
nishuChandravanshi <nishu181099@gmail.comgit config --global user.email nishu181099@gmail.comgit config --global user.email nishu181099@gmail.com>
2020-07-24 21:49:47 +0530

    Merge Two Sorted lists, T:O(total length of final list), S: O(1)

--- Linked List/LC:21- Merge two Sorted Lists.cpp
+++ Linked List/LC:21- Merge two Sorted Lists.cpp
@@ -12,7 +12,9 @@ public:
       
 	if(l1==NULL ) return l2;
         if(l2==NULL) return l1;
-        
+
+	//return merge(l1, l2);        
+
         ListNode res(0);
         ListNode * curr= &res;
         
@@ -36,10 +38,36 @@ public:
         
     }
 };
+/*
+	//Recursive
+
+    ListNode* merge(ListNode* l1, ListNode* l2)
+    {
+        if(l1 == NULL) return l2;
+        if(l2 == NULL) return l1;
+        
+        ListNode* temp = NULL;
+        
+        if(l1->val < l2->val)
+        {
+            temp = l1;
+            temp->next = merge(l1->next, l2);
+        }
+        else 
+        {
+            temp = l2;
+            temp->next = merge(l1, l2->next);
+        }
+        
+        return temp;
+    }
+*/
 
 /*
 	T: O(n+m), S:O(1)
 idea-
 -res is dummy-head whose next will point to the overall merged list
-- next of curr will always point to the list with smaller head value, and l1 and l2 will be moved acgly
+-to maintain sorted list till curr in every iteration 
+(via next of curr always pointing to the list with smaller head value, then updating curr to new sorted pos(ie curr=curr->next)
+- l1 and l2 will be moved acgly
 */
