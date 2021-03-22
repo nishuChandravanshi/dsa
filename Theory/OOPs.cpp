51ced8d42045937894d57d69916d3516f9204665
nishuChandravanshi <nishu181099@gmail.com>
2021-03-22 15:40:34 +0530

    oops,py/lib files modified

--- Theory/OOPs.cpp
+++ Theory/OOPs.cpp
@@ -682,6 +682,11 @@ types-
 2.Abstraction -
     -Abstraction means displaying only essential information and hiding the details. 
     Data abstraction refers to providing only essential information about the data to the outside world, hiding the background details or implementation.
+    ie->
+    -Abstraction is a process where you show only “relevant” data and “hide” unnecessary details of an object 
+    from the user. Consider your mobile phone, you just need to know what buttons are to be pressed to send 
+    a message or make a call, What happens when you press a button, how your messages are sent, how your calls 
+    are connected is all abstracted away from the user.
 
     eg-
         Consider a real-life example of a man driving a car. The man only knows that pressing the accelerators will increase the speed of the car or applying brakes will stop the car 
@@ -727,6 +732,13 @@ types-
     - Its the process of wrapping up of Data and Functions inside a single Entity (usually
     Classes)
     - Encapsulation helps to establish Abstraction and Data Hiding.
+    or->
+    -Encapsulation is the process of combining data and functions into a single unit called class. 
+    In Encapsulation, the data is not accessed directly; it is accessed through the functions present 
+    inside the class. In simpler words, 
+    attributes of the class are kept private and public getter and setter methods are provided 
+    to manipulate these attributes. Thus, encapsulation makes the concept of data hiding possible.
+
 
 4. Polymorphism -
     Polymorphism means the ability to take more than one form. An operation may exhibit different
